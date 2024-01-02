750c739e7ab1ea329fa4fdd31206e954e52d738b
faez <benfaez07@gmail.com>
2024-01-02 18:47:39 +0100

    Add new component

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,9 @@
 import calculateDistance from "./calculateDistance.js";
 import calculateCenter from "./calculateCenter.js";
 import isWithinDistance from "./isWithinDistance.js";
-import sortSpotsByProximity from "./sortSpotsByProximity.js";
 
 export {
     calculateDistance,
     calculateCenter,
     isWithinDistance,
-    sortSpotsByProximity
 };
