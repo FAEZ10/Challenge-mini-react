897062caee9cffd3a0642c8c7d5b2a7b53ec68b9
Ibsaidou <isaidou2@myges.fr>
2024-01-03 16:47:32 +0100

    home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import {CreateElement} from 'mini-react';
+
 import MiniReactDOM from 'mini-react-dom';
 import {BrowserRouter} from './components/Router/';
 import routes from './routes'; 
+import './styles/index.scss';
+
 
 const App = CreateElement({
   componentClass: BrowserRouter,
