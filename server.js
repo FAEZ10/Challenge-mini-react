f2aa3096adf538fe685e9a39e779939d7b2cb85b
faez <benfaez07@gmail.com>
2024-01-05 00:52:50 +0100

    Fix module system for Heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
 const app = express();
 
-// Servir les fichiers statiques du dossier 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-// Rediriger toutes les routes vers 'index.html'
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
